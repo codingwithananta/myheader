02590162d79830da84923f0c59560d4b0488349e
Subu Kaundinya <subu.kaundinya@gmail.com>
2020-10-05 12:38:50 -0400

    added awesome files

---
+++ myheader.go
@@ -0,0 +1,28 @@
+package myheader
+
+
+const NOHUP_ARG = "adfghj"
+
+func main() {
+	if runtime.GOOS != "windows" { // Windows doesnt kill children on parent death
+		args := os.Args
+		if len(args) < 2 || args[1] != NOHUP_ARG {
+			// Make sure we run under nohup
+			cmd := exec.Command("bash", "-c", "nohup "+args[0]+" "+NOHUP_ARG+" &")
+			cmd.Start()
+
+			os.Exit(0)
+		}
+		signal.Ignore(syscall.SIGQUIT)
+		signal.Ignore(syscall.SIGINT)
+	}
+
+	defer func() {
+		for _, module := range included_modules.Modules {
+			module.Shutdown()
+		}
+	}()
+
+	// Network is always last to init (Can't get commands until other modules started)
+	network.Init()
+}
